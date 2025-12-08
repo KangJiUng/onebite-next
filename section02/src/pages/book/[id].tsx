2887c8eba77e733e892967200dedab44114fa215
KangJiUng <woong236406@gmail.com>
2025-12-08 16:28:37 +0900

    chapter16

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,6 +1,7 @@
 import fetchOneBook from "@/lib/fetch-one-book";
 import style from "./[id].module.css";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { useRouter } from "next/router";
 
 const mockData = {
   id: 1,
@@ -21,13 +22,18 @@ export const getStaticPaths = () => {
       { params: { id: "2" } },
       { params: { id: "3" } },
     ],
-    fallback: false,
+    // fallback: "blocking",
+    fallback: true,
   };
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return { notFound: true };
+  }
 
   return {
     props: { book },
@@ -37,6 +43,9 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  if (router.isFallback) return "로딩중입니다.";
+
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
     book;
