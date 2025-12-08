bf0066204cb3af6d2ecc2c325bfa819533e4618f
KangJiUng <woong236406@gmail.com>
2025-12-08 17:01:44 +0900

    chapter20

--- section02/src/lib/fetch-random-books.ts
+++ section02/src/lib/fetch-random-books.ts
@@ -1,7 +1,7 @@
 import { BookData } from "@/types";
 
 export default async function fetchRandomBooks(): Promise<BookData[]> {
-  const url = `http://localhost:12345/book`;
+  const url = `https://onebite-books-server-main-roan-three.vercel.app/book`;
 
   try {
     const response = await fetch(url);
